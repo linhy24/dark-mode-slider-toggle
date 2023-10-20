a6930a4a796cf8394949c2311d1036ed79d1585c
Hongyang Lin <57543434+linhy24@users.noreply.github.com>
2023-10-20 14:25:05 -0500

    initial project setup

---
+++ darkmode.js
@@ -0,0 +1,15 @@
+/*
+    Implement logic of darkmode toggle
+*/
+
+// Control theme preference by diabling corresponding CSS file
+var e = document.getElementById("dark-mode-toggle-5");
+e.addEventListener("colorschemechange", function() {
+    if (e.getAttribute("mode") == "dark") {
+        document.querySelector('link[title="dark"]').disabled = false;
+        document.querySelector('link[title="light"]').disabled = true;
+    } else {
+        document.querySelector('link[title="dark"]').disabled = true;
+        document.querySelector('link[title="light"]').disabled = false;
+    }
+})
