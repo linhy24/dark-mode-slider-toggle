7187a12abba5ff063bcf0d2e5d61a367a5114a89
Hongyang Lin <57543434+linhy24@users.noreply.github.com>
2023-10-20 21:31:34 -0500

    fix auto background change when switch to dark theme

--- script/darkmode.js
+++ script/darkmode.js
@@ -2,6 +2,11 @@
     Implement logic of darkmode toggle
 */
 
+// if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//     document.querySelector('link[title="dark"]').disabled = false;
+//     document.querySelector('link[title="light"]').disabled = true;
+// }
+
 // Control theme preference by diabling corresponding CSS file
 var e = document.getElementById("dark-mode-toggle-5");
 e.addEventListener("colorschemechange", function() {
